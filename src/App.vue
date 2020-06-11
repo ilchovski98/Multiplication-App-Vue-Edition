0086d92915ffb6a40b8d3204c83b2c699c3c17d1
ilchovski98 <ilchovski67bg@gmail.com>
2020-06-11 23:28:14 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <multiplication-template></multiplication-template>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MultiplicationTemplate from './components/multiplicationTemplate'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MultiplicationTemplate
   }
 }
 </script>
---
+++ src/components/multiplicationTemplate/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>Hey</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MultiplicationTemplate",
+    props: [],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
