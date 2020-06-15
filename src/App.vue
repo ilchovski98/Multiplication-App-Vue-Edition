1611b45340725472d315a687540b8c11ac841780
ilchovski98 <ilchovski67bg@gmail.com>
2020-06-15 21:13:47 +0300

    themes fixed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <header class="header">
+    <header :class="headerClass">
       <h1 class="title">{{ title }}</h1>
     </header>
 
     <div class="navigator">
-      <button class="btn" @click="component='InfiniteGame'; count=1; title='Infinity Mode'">Infinity</button>
-      <button class="btn" @click="component='TrackGame'; count=1; title='Time Mode' ">For Time</button>
+      <button class="btn" @click="component='InfiniteGame'; infinityTheme()">Infinity</button>
+      <button class="btn" @click="component='TrackGame'; trackTheme()">For Time</button>
       <button class="btn">Double Digits</button>
     </div>
-
-    <div class="backgrounds">
+    
+    <div :class="bodyClass">
       <component  v-bind:is="component" :count="count" :addComponent="addComponent"></component>
     </div>
 
@@ -34,14 +34,30 @@ export default {
     return {
       count: 1,
       component: 'InfiniteGame',
-      title: 'Infinity Mode'
+      title: 'Infinity Mode',
+      headerClass: 'headerInfinity',
+      bodyClass: 'backgroundsInfinity'
     }
   },
   methods: {
     addComponent() {
       this.count += 1;
+    },
+    infinityTheme() {
+      this.headerClass = 'headerInfinity';
+      this.bodyClass = 'backgroundsInfinity';
+      this.title = 'Infinity Mode';
+      this.count = 1;
+
+    },
+    trackTheme() {
+      this.headerClass = 'headerTrack';
+      this.bodyClass = 'backgroundsTrack';
+      this.title = 'Time Mode';
+      this.count = 1;
     }
-  }
+  },
+  
 }
 </script>
 
@@ -70,11 +86,19 @@ export default {
   
 }
 
-.header {
+.headerInfinity {
   display: flex;
   justify-content: center;
   height: 22vh;
   background-image: url("./assets/backgrounds/09.jpg");
+  background-size: cover;
+}
+
+.headerTrack {
+  display: flex;
+  justify-content: center;
+  height: 22vh;
+  background-image: url("./assets/backgrounds/10.jpg");
   background-size: cover;
 }
 
@@ -84,8 +108,15 @@ export default {
   text-shadow: 0 0 2px black;
 }
 
-.backgrounds {
-  background-image: url("./assets/backgrounds/09-double-opacity.jpg");
+.backgroundsInfinity {
+  background-image: url("./assets/backgrounds/09-opacity.jpg");
+  background-size: cover;
+  height: 100vh;
+  box-shadow: 0px 6px 8px 3px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.backgroundsTrack {
+  background-image: url("./assets/backgrounds/10-opacity.jpg");
   background-size: cover;
   height: 100vh;
   box-shadow: 0px 6px 8px 3px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
